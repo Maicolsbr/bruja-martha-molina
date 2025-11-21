16e16a5820e52af883a192eb31703b569e7a2505
Maicolsbr <108235522+Maicolsbr@users.noreply.github.com>
2025-11-21 12:44:16 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // 1. **CONFIGURA AQUÍ TU NÚMERO DE WHATSAPP Y MENSAJE**
-    const whatsappNumber = '573222472882'; // Reemplaza con tu código de país y número. Ejemplo: 57 (Colombia)
+    const whatsappNumber = '+573144222492'; // Reemplaza con tu código de país y número. Ejemplo: 57 (Colombia)
     const whatsappMessage = 'Hola Martha Molina, vengo de tu página web y quiero una consulta sobre Amarres de Amor.';
 
     // Codifica el mensaje para la URL
